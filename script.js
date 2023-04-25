0ec1489271d10fce4e8de8ad34eff59d3a005b88
salikwani <120441251+salikwani@users.noreply.github.com>
2023-04-25 19:19:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function createTable() {
     //Write your code here
+	var row = prompt("Enter Number of rows");
+	var column = prompt("Enter Number of column");
+	for(var i=0;i<row;i++) {
+		var r = document.createElement("tr");
+		for(var j=0;j<column;j++) {
+			var c = document.createElement("td");
+			c.innerText = `Row-${i} Column-${j}`;
+			r.appendChild(c);
+		}
+		document.querySelector("#myTable").appendChild(r);
+	}
   
 }
